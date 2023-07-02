727479fac88edb54f5bb38c45f3847068b7136e4
ShabirK21 <shaber213@gmail.com>
2023-07-02 12:57:18 +0800

    added tailwindcss styling

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
 //items in the global namespace are accessible throught out the node application
-global.db = new sqlite3.Database('./database.db',function(err){
-  if(err){
+global.db = new sqlite3.Database("./database.db", function (err) {
+  if (err) {
     console.error(err);
     process.exit(1); //Bail out we can't connect to the DB
-  }else{
+  } else {
     console.log("Database connected");
     global.db.run("PRAGMA foreign_keys=ON"); //This tells SQLite to pay attention to foreign key constraints
   }
 });
 
-
-const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
 
 //set the app to use ejs for rendering
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 //this adds all the userRoutes to the app under the path /user
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
