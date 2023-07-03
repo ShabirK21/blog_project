ebf3fb28f2abaa2b9e068ff1ddfdf0b8399c1fef
ShabirK21 <shaber213@gmail.com>
2023-07-03 13:55:25 +0800

    created login page for admins, prevented page caching

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 3000;
 const sqlite3 = require("sqlite3").verbose();
 
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+app.use(express.urlencoded({ extended: true }));
 //items in the global namespace are accessible throught out the node application
 global.db = new sqlite3.Database("./database.db", function (err) {
   if (err) {
@@ -20,16 +25,78 @@ const adminRoutes = require("./routes/admin");
 app.set("view engine", "ejs");
 //use public folder for static files
 app.use(express.static("public"));
+
 //user landing page
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.use(
+  session({
+    secret: "abc123abc123qwe123",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+passport.serializeUser((admin, done) => {
+  done(null, admin.id);
+});
+
+passport.deserializeUser((id, done) => {
+  global.db.get("SELECT * FROM admins WHERE id = ?", id, (err, admin) => {
+    if (err) {
+      console.error("Error deserializing user:", err);
+      return done(err);
+    }
+    done(null, admin);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //this adds all the userRoutes to the app under the path /user
 //app.use("/user", userRoutes);
 //this adds all the adminRoutes to the app under the path /admin
 app.use("/admin", adminRoutes);
 
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    global.db.get(
+      "SELECT * FROM admins WHERE username = ?",
+      username,
+      (err, admin) => {
+        if (err) return done(err);
+        if (!admin) return done(null, false);
+        bcrypt.compare(password, admin.password, (err, res) => {
+          if (err) return done(err);
+          if (!res) return done(null, false);
+          return done(null, admin);
+        });
+      }
+    );
+  })
+);
+
+/*
+const username = "admin";
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+const password = "password123";
+const id = 1;
+const hash = bcrypt.hashSync(password, salt);
+const query = `INSERT INTO admins (username, password) VALUES (?, ?)`;
+global.db.run(query, [username, hash], function (err) {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log(`Admin user created with ID: ${this.lastID}`);
+  }
+});
+
+global.db.close();
+*/
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- routes/admin.js
+++ routes/admin.js
@@ -2,18 +2,54 @@
 const express = require("express");
 const router = express.Router();
 const assert = require("assert");
+const passport = require("passport");
 
-router.get("/", (req, res, next) => {
+router.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log("Error logging out", err);
+    }
+    res.redirect("/admin/login");
+  });
+});
+
+router.get("/login", (req, res) => {
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.render("admin-login", { user: req.user });
+});
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      // Authentication failed
+      console.log("Authentication failed");
+      return res.redirect("/admin/login");
+    }
+    // Authentication successful
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/admin");
+    });
+  })(req, res, next);
+});
+
+router.get("/", ensureAuthenticated, (req, res, next) => {
   global.db.all("SELECT * FROM articles", function (err, rows) {
     if (err) {
       next(err); //send the error on to the error handler
     } else {
-      res.render("admin-home", { articles: rows });
+      res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+      res.render("admin-home", { articles: rows, user: req.user });
     }
   });
 });
 // delete article from database
-router.post("/articles/:id/delete", (req, res, next) => {
+router.post("/articles/:id/delete", ensureAuthenticated, (req, res, next) => {
   const articleId = req.params.id;
   global.db.run("DELETE FROM articles WHERE id = ?", articleId, function (err) {
     if (err) {
@@ -24,7 +60,7 @@ router.post("/articles/:id/delete", (req, res, next) => {
   });
 });
 // publish article
-router.post("/articles/:id/publish", (req, res, next) => {
+router.post("/articles/:id/publish", ensureAuthenticated, (req, res, next) => {
   const articleId = req.params.id;
   global.db.run(
     "UPDATE articles SET draft_published = 'published' WHERE id = ?",
@@ -39,12 +75,19 @@ router.post("/articles/:id/publish", (req, res, next) => {
   );
 });
 
-router.get("/settings", (req, res) => {
+router.get("/settings", ensureAuthenticated, (req, res) => {
   res.render("admin-settings");
 });
 
-router.get("/edit-articles", (req, res) => {
+router.get("/edit-articles", ensureAuthenticated, (req, res) => {
   res.render("admin-edit-article");
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/admin/login");
+}
 
 module.exports = router;
