018097de0b8db62e33fbdaabbf720c629322ab31
ShabirK21 <shaber213@gmail.com>
2023-07-05 18:30:33 +0800

    updates

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,99 @@ const express = require("express");
 const router = express.Router();
 const assert = require("assert");
 const passport = require("passport");
+let blog_settings = null;
+
+// home page
+router.get("/", ensureAuthenticated, (req, res, next) => {
+  fetchBlogSettings(function (err, settings) {
+    if (err) {
+      next(err); //send the error on to the error handler
+    } else {
+      global.db.all("SELECT * FROM articles", function (err, rows) {
+        if (err) {
+          next(err); //send the error on to the error handler
+        } else {
+          res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+          res.render("admin-home", {
+            articles: rows,
+            user: req.user,
+            activePage: "admin",
+            blog_settings: settings, // Use the retrieved settings instead of blog_settings variable
+          });
+        }
+      });
+    }
+  });
+});
+
+// settings page
+router.get("/settings", ensureAuthenticated, (req, res, next) => {
+  global.db.all("SELECT * FROM blog_settings", function (err, rows) {
+    if (err) {
+      next(err); //send the error on to the error handler
+    } else {
+      res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+      res.render("admin-settings", { blog_settings: rows });
+    }
+  });
+});
+
+router.post("/update-settings", ensureAuthenticated, (req, res, next) => {
+  const newTitle = req.body.title;
+  const newSubtitle = req.body.subtitle;
+  const newAuthor = req.body.author;
+  global.db.run(
+    "UPDATE blog_settings SET title = ?, subtitle = ?, author = ? ",
+    [newTitle, newSubtitle, newAuthor],
+    function (err) {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect("/admin/settings");
+      }
+    }
+  );
+});
+
+// article editing page
+router.get("/edit-articles/:id?", ensureAuthenticated, (req, res, next) => {
+  const articleId = req.params.id;
+  fetchBlogSettings(function (err, settings) {
+    if (err) {
+      next(err);
+    } else {
+      if (articleId) {
+        // Existing article, fetch data from the database
+        global.db.get(
+          "SELECT * FROM articles WHERE id = ?",
+          articleId,
+          function (err, row) {
+            if (err) {
+              next(err); // Send the error to the error handler
+            } else {
+              res.setHeader(
+                "Cache-Control",
+                "no-cache, no-store, must-revalidate"
+              );
+              res.render("admin-edit-article", {
+                blog_settings: settings,
+                article: row,
+              });
+            }
+          }
+        );
+      } else {
+        // New draft, no article ID provided
+        const emptyArticle = { title: "", subtitle: "", content: "" };
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.render("admin-edit-article", {
+          blog_settings: settings,
+          article: emptyArticle,
+        });
+      }
+    }
+  });
+});
 
 router.post("/logout", (req, res) => {
   req.session.destroy((err) => {
@@ -11,6 +104,52 @@ router.post("/logout", (req, res) => {
     }
     res.redirect("/admin/login");
   });
+});
+
+router.post("/add-article", ensureAuthenticated, (req, res, next) => {
+  const title = req.body.title;
+  const subtitle = req.body.subtitle;
+  const content = req.body.content;
+  global.db.run(
+    "INSERT INTO articles(title,subtitle,content,author,publication_date, last_modified, draft_or_published,image_path) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+    title,
+    subtitle,
+    content,
+    req.user.username,
+    formatDate(Date.now()),
+    formatDate(Date.now()),
+    "draft",
+    "",
+    function (err) {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect("/admin");
+      }
+    }
+  );
+});
+
+router.post("/update-article/:id", ensureAuthenticated, (req, res, next) => {
+  const title = req.body.title;
+  const subtitle = req.body.subtitle;
+  const content = req.body.content;
+  const articleId = req.params.id;
+  global.db.run(
+    "UPDATE articles SET title = ?, subtitle = ?, content = ?, last_modified = ? WHERE id = ?",
+    title,
+    subtitle,
+    content,
+    formatDate(Date.now()),
+    articleId,
+    function (err) {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect("/admin");
+      }
+    }
+  );
 });
 
 router.get("/login", (req, res) => {
@@ -38,16 +177,6 @@ router.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
-router.get("/", ensureAuthenticated, (req, res, next) => {
-  global.db.all("SELECT * FROM articles", function (err, rows) {
-    if (err) {
-      next(err); //send the error on to the error handler
-    } else {
-      res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-      res.render("admin-home", { articles: rows, user: req.user });
-    }
-  });
-});
 // delete article from database
 router.post("/articles/:id/delete", ensureAuthenticated, (req, res, next) => {
   const articleId = req.params.id;
@@ -63,7 +192,7 @@ router.post("/articles/:id/delete", ensureAuthenticated, (req, res, next) => {
 router.post("/articles/:id/publish", ensureAuthenticated, (req, res, next) => {
   const articleId = req.params.id;
   global.db.run(
-    "UPDATE articles SET draft_published = 'published' WHERE id = ?",
+    "UPDATE articles SET draft_or_published = 'published' WHERE id = ?",
     articleId,
     function (err) {
       if (err) {
@@ -75,19 +204,42 @@ router.post("/articles/:id/publish", ensureAuthenticated, (req, res, next) => {
   );
 });
 
-router.get("/settings", ensureAuthenticated, (req, res) => {
-  res.render("admin-settings");
-});
-
-router.get("/edit-articles", ensureAuthenticated, (req, res) => {
-  res.render("admin-edit-article");
-});
+function fetchBlogSettings(callback) {
+  if (blog_settings) {
+    // If the blog settings are already retrieved, return them immediately
+    callback(null, blog_settings);
+  } else {
+    global.db.all("SELECT * FROM blog_settings", function (err, rows) {
+      if (err) {
+        console.log(err);
+        callback(err, null); // Pass the error to the callback
+      } else {
+        blog_settings = rows; // Store the retrieved rows in the global variable
+        callback(null, rows); // Pass the rows to the callback
+      }
+    });
+  }
+}
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/admin/login");
+}
+
+function formatDate(date) {
+  const d = new Date(date);
+
+  // Get day, month, and year
+  const day = String(d.getDate()).padStart(2, "0");
+  const month = String(d.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const year = d.getFullYear();
+
+  // Format the date as dd-mm-yyyy
+  const formattedDate = `${day}/${month}/${year}`;
+
+  return formattedDate;
 }
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,106 +1,74 @@
-
-/**
- * These are example routes for user management
- * This shows how to correctly structure your routes for the project
- */
-
+// Imports
 const express = require("express");
 const router = express.Router();
-const assert = require('assert');
+const assert = require("assert");
 
-/**
- * @desc retrieves the current users
- */
-router.get("/get-test-users", (req, res, next) => {
+router.get("/article/:id", (req, res, next) => {
+  const articleId = req.params.id;
 
-  //Use this pattern to retrieve data
-  //NB. it's better NOT to use arrow functions for callbacks with this library
-  global.db.all("SELECT * FROM testUsers", function (err, rows) {
-    if (err) {
-      next(err); //send the error on to the error handler
-    } else {
-      res.json(rows);
-    }
-  });
-  
-});
-
-/**
- * @desc retrieves the current user records
- */
-router.get("/get-user-records", (req, res, next) => {
-  //USE this pattern to retrieve data
-  //NB. it's better NOT to use arrow functions for callbacks with this library
-
-  global.db.all("SELECT * FROM testUserRecords", function (err, rows) {
-    if (err) {
-      next(err); //send the error on to the error handler
-    } else {
-      res.json(rows);
-    }
-  });
-});
-
-/**
- * @desc Renders the page for creating a user record
- */
-router.get("/create-user-record", (req, res) => {
-  res.render("create-user-record");
-});
-
-/**
- * @desc Add a new user record to the database for user id = 1
- */
-router.post("/create-user-record", (req, res, next) => {
-  //USE this pattern to update and insert data
-  //NB. it's better NOT to use arrow functions for callbacks with this library
-  const data = generateRandomData(10);
-  global.db.run(
-    "INSERT INTO testUserRecords ('test_record_value', 'test_user_id') VALUES( ?, ? );",
-    [data, 1],
-    function (err) {
+  global.db.get(
+    "SELECT * FROM articles WHERE id = ?",
+    articleId,
+    function (err, article_row) {
       if (err) {
-        next(err); //send the error on to the error handler
+        next(err); // send the error to the error handler
       } else {
-        res.send(`New data inserted @ id ${this.lastID}!`);
-        next();
+        global.db.all("SELECT * FROM blog_settings", function (err, settings) {
+          if (err) {
+            next(err); // send the error to the error handler
+          } else {
+            global.db.all(
+              "SELECT * FROM userComments WHERE article_id = ?",
+              articleId,
+              function (err, comments) {
+                if (err) {
+                  next(err); // send the error to the error handler
+                } else {
+                  res.render("user-article", {
+                    article: article_row,
+                    blog_settings: settings,
+                    comments: comments,
+                  });
+                }
+              }
+            );
+          }
+        });
       }
     }
   );
 });
 
-///////////////////////////////////////////// HELPERS ///////////////////////////////////////////
-
-/**
- * @desc A helper function to generate a random string
- * @returns a random lorem ipsum string
- */
-function generateRandomData(numWords = 5) {
-  const str =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
-
-  const words = str.split(" ");
-
-  let output = "";
-
-  for (let i = 0; i < numWords; i++) {
-    output += choose(words);
-    if (i < numWords - 1) {
-      output += " ";
+router.post("/like/:id", (req, res, next) => {
+  const articleId = req.params.id;
+  global.db.run(
+    "UPDATE articles SET likes = likes + 1 WHERE id = ?",
+    articleId,
+    function (err) {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect("/");
+      }
     }
-  }
+  );
+});
 
-  return output;
-}
-
-/**
- * @desc choose and return an item from an array
- * @returns the item
- */
-function choose(array) {
-  assert(Array.isArray(array), "Not an array");
-  const i = Math.floor(Math.random() * array.length);
-  return array[i];
-}
+router.post("/comment/:id", (req, res, next) => {
+  const articleId = req.params.id;
+  const comment = req.body.comment;
+  global.db.run(
+    "INSERT INTO userComments (article_id,comment) VALUES (?, ?)",
+    articleId,
+    comment,
+    function (err) {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect("/user/article/" + articleId);
+      }
+    }
+  );
+});
 
 module.exports = router;
