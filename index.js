53385d6615ba14e582cbb9de67694549c6cd8017
ShabirK21 <shaber213@gmail.com>
2023-07-05 18:28:17 +0800

    updated landing page

--- index.js
+++ index.js
@@ -23,15 +23,30 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 //user landing page
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/", (req, res, next) => {
+  global.db.all("SELECT * FROM articles", function (err, articleRows) {
+    if (err) {
+      next(err); // send the error to the error handler
+    } else {
+      global.db.all("SELECT * FROM blog_settings", function (err, settings) {
+        if (err) {
+          next(err); // send the error to the error handler
+        } else {
+          res.render("user-home", {
+            articles: articleRows,
+            blog_settings: settings,
+          });
+        }
+      });
+    }
+  });
 });
 
 // require admin authentication
 require("./admin-authentication")(app, global.db);
 
 //this adds all the userRoutes to the app under the path /user
-//app.use("/user", userRoutes);
+app.use("/user", userRoutes);
 //this adds all the adminRoutes to the app under the path /admin
 app.use("/admin", adminRoutes);
 
